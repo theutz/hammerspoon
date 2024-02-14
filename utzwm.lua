20c01fb8af9d0bda367174da5901b9f3c7a2b0f2
Mike Utz <mike.u@delegator.com>
2024-02-14 10:15:00 +0300

    chore: reformat
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- utzwm.lua
+++ utzwm.lua
@@ -4,10 +4,10 @@ local wf = hs.window.filter
 local mods = { "ctrl", "alt", "cmd" }
 
 spoon.SpoonInstall:andUse("WindowScreenLeftAndRight", {
-  hotkeys = {
-    screen_left = { mods, "[" },
-    screen_right = { mods, "]" },
-  },
+	hotkeys = {
+		screen_left = { mods, "[" },
+		screen_right = { mods, "]" },
+	},
 })
 
 local gridSize = hs.geometry.size(12, 8) or {}
@@ -15,203 +15,203 @@ local gridMargin = hs.geometry.size(16, 16) or {}
 
 hs.grid.setGrid(gridSize).setMargins(gridMargin)
 
-hs.hotkey.bind(mods, "/", function()
-  local grid = hs.grid.getGrid()
-  hs.grid.setGrid("6x4")
-  hs.grid.show(function()
-    hs.grid.setGrid(grid)
-  end)
+hs.hotkey.bind(mods, "x", function()
+	local grid = hs.grid.getGrid()
+	hs.grid.setGrid("6x4")
+	hs.grid.show(function()
+		hs.grid.setGrid(grid)
+	end)
 end)
 
 hs.hotkey.bind(mods, "h", function()
-  ---@type hs.window
-  local win = hs.window.frontmostWindow()
-  local current = hs.grid.get(win)
+	---@type hs.window
+	local win = hs.window.frontmostWindow()
+	local current = hs.grid.get(win)
 
-  local initial = hs.geometry.new("0,0 6x8")
+	local initial = hs.geometry.new("0,0 6x8")
 
-  ---@type table
-  local big = initial:copy()
-  big.w = big.w + 2
+	---@type table
+	local big = initial:copy()
+	big.w = big.w + 2
 
-  ---@type table
-  local small = initial:copy()
-  small.w = small.w - 2
+	---@type table
+	local small = initial:copy()
+	small.w = small.w - 2
 
-  local target = initial
+	local target = initial
 
-  if initial == current then
-    target = small
-  elseif current == small then
-    target = big
-  end
+	if initial == current then
+		target = small
+	elseif current == small then
+		target = big
+	end
 
-  hs.grid.set(win, target)
+	hs.grid.set(win, target)
 end)
 
 hs.hotkey.bind(mods, "l", function()
-  local win = hs.window.frontmostWindow()
-  local current = hs.grid.get(win)
+	local win = hs.window.frontmostWindow()
+	local current = hs.grid.get(win)
 
-  local initial = hs.geometry.new("6,0 6x8")
+	local initial = hs.geometry.new("6,0 6x8")
 
-  ---@type table
-  local big = initial:copy()
-  big.w = big.w + 2
-  big.x = big.x - 2
+	---@type table
+	local big = initial:copy()
+	big.w = big.w + 2
+	big.x = big.x - 2
 
-  ---@type table
-  local small = initial:copy()
-  small.w = small.w - 2
-  small.x = small.x + 2
+	---@type table
+	local small = initial:copy()
+	small.w = small.w - 2
+	small.x = small.x + 2
 
-  local target = initial
+	local target = initial
 
-  if current == initial then
-    target = small
-  elseif current == small then
-    target = big
-  end
+	if current == initial then
+		target = small
+	elseif current == small then
+		target = big
+	end
 
-  hs.grid.set(win, target)
+	hs.grid.set(win, target)
 end)
 
 hs.hotkey.bind(mods, "k", function()
-  local win = hs.window.frontmostWindow()
-  local current = hs.grid.get(win)
+	local win = hs.window.frontmostWindow()
+	local current = hs.grid.get(win)
 
-  local initial = hs.geometry.new("0,0 12x4")
+	local initial = hs.geometry.new("0,0 12x4")
 
-  ---@type table
-  local right_quarter = initial:copy()
-  right_quarter.w = 6
-  right_quarter.x = 6
+	---@type table
+	local right_quarter = initial:copy()
+	right_quarter.w = 6
+	right_quarter.x = 6
 
-  ---@type table
-  local left_quarter = initial:copy()
-  left_quarter.w = 6
-  left_quarter.x = 0
+	---@type table
+	local left_quarter = initial:copy()
+	left_quarter.w = 6
+	left_quarter.x = 0
 
-  ---@type table
-  local right_sixth = initial:copy()
-  right_sixth.w = 4
-  right_sixth.x = 0
+	---@type table
+	local right_sixth = initial:copy()
+	right_sixth.w = 4
+	right_sixth.x = 0
 
-  ---@type table
-  local center_sixth = initial:copy()
-  center_sixth.w = 4
-  center_sixth.x = 4
+	---@type table
+	local center_sixth = initial:copy()
+	center_sixth.w = 4
+	center_sixth.x = 4
 
-  ---@type table
-  local left_sixth = initial:copy()
-  left_sixth.w = 4
-  left_sixth.x = 8
+	---@type table
+	local left_sixth = initial:copy()
+	left_sixth.w = 4
+	left_sixth.x = 8
 
-  local target = initial
+	local target = initial
 
-  if current == initial then
-    target = left_quarter
-  elseif current == left_quarter then
-    target = right_quarter
-  elseif current == right_quarter then
-    target = right_sixth
-  elseif current == right_sixth then
-    target = center_sixth
-  elseif current == center_sixth then
-    target = left_sixth
-  end
+	if current == initial then
+		target = left_quarter
+	elseif current == left_quarter then
+		target = right_quarter
+	elseif current == right_quarter then
+		target = right_sixth
+	elseif current == right_sixth then
+		target = center_sixth
+	elseif current == center_sixth then
+		target = left_sixth
+	end
 
-  hs.grid.set(win, target)
+	hs.grid.set(win, target)
 end)
 
 hs.hotkey.bind(mods, "j", function()
-  local win = hs.window.frontmostWindow()
-  local current = hs.grid.get(win)
+	local win = hs.window.frontmostWindow()
+	local current = hs.grid.get(win)
 
-  local initial = hs.geometry.new("0,4 12x4")
+	local initial = hs.geometry.new("0,4 12x4")
 
-  ---@type table
-  local right_quarter = initial:copy()
-  right_quarter.w = 6
-  right_quarter.x = 6
+	---@type table
+	local right_quarter = initial:copy()
+	right_quarter.w = 6
+	right_quarter.x = 6
 
-  ---@type table
-  local left_quarter = initial:copy()
-  left_quarter.w = 6
-  left_quarter.x = 0
+	---@type table
+	local left_quarter = initial:copy()
+	left_quarter.w = 6
+	left_quarter.x = 0
 
-  ---@type table
-  local right_sixth = initial:copy()
-  right_sixth.w = 4
-  right_sixth.x = 0
+	---@type table
+	local right_sixth = initial:copy()
+	right_sixth.w = 4
+	right_sixth.x = 0
 
-  ---@type table
-  local center_sixth = initial:copy()
-  center_sixth.w = 4
-  center_sixth.x = 4
+	---@type table
+	local center_sixth = initial:copy()
+	center_sixth.w = 4
+	center_sixth.x = 4
 
-  ---@type table
-  local left_sixth = initial:copy()
-  left_sixth.w = 4
-  left_sixth.x = 8
+	---@type table
+	local left_sixth = initial:copy()
+	left_sixth.w = 4
+	left_sixth.x = 8
 
-  local target = initial
+	local target = initial
 
-  if current == initial then
-    target = left_quarter
-  elseif current == left_quarter then
-    target = right_quarter
-  elseif current == right_quarter then
-    target = right_sixth
-  elseif current == right_sixth then
-    target = center_sixth
-  elseif current == center_sixth then
-    target = left_sixth
-  end
+	if current == initial then
+		target = left_quarter
+	elseif current == left_quarter then
+		target = right_quarter
+	elseif current == right_quarter then
+		target = right_sixth
+	elseif current == right_sixth then
+		target = center_sixth
+	elseif current == center_sixth then
+		target = left_sixth
+	end
 
-  hs.grid.set(win, target)
+	hs.grid.set(win, target)
 end)
 
 hs.hotkey.bind(mods, "return", function()
-  local win = hs.window.frontmostWindow()
-  local current = hs.grid.get(win)
+	local win = hs.window.frontmostWindow()
+	local current = hs.grid.get(win)
 
-  local initial = hs.geometry.new({ 0, 0, gridSize.w, gridSize.h })
+	local initial = hs.geometry.new({ 0, 0, gridSize.w, gridSize.h })
 
-  ---@type table
-  local two_thirds = initial:copy()
-  two_thirds.w = two_thirds.w - 4
-  two_thirds.x = two_thirds.x + 2
+	---@type table
+	local two_thirds = initial:copy()
+	two_thirds.w = two_thirds.w - 4
+	two_thirds.x = two_thirds.x + 2
 
-  ---@type table
-  local half = initial:copy()
-  half.w = half.w - 6
-  half.x = half.x + 3
+	---@type table
+	local half = initial:copy()
+	half.w = half.w - 6
+	half.x = half.x + 3
 
-  ---@type table
-  local third = initial:copy()
-  third.w = third.w - 8
-  third.x = third.x + 4
+	---@type table
+	local third = initial:copy()
+	third.w = third.w - 8
+	third.x = third.x + 4
 
-  local target = initial
+	local target = initial
 
-  if current == initial then
-    target = two_thirds
-  elseif current == two_thirds then
-    target = half
-  elseif current == half then
-    target = third
-  end
+	if current == initial then
+		target = two_thirds
+	elseif current == two_thirds then
+		target = half
+	elseif current == half then
+		target = third
+	end
 
-  hs.grid.set(win, target)
+	hs.grid.set(win, target)
 end)
 
 hs.hotkey.bind(mods, "space", function()
-  local win = hs.window.frontmostWindow()
-  ---@type table
-  local frame = win:centerOnScreen():frame()
-  frame.y = frame.y + gridMargin.h - 3 -- not sure why we need this 3, but we do
-  win:move(frame)
+	local win = hs.window.frontmostWindow()
+	---@type table
+	local frame = win:centerOnScreen():frame()
+	frame.y = frame.y + gridMargin.h - 3 -- not sure why we need this 3, but we do
+	win:move(frame)
 end)
 
 -- AutoStashApps = wf.new({
