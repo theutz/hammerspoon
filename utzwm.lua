939f48e567dfee8e308085f1da17d4a3f01473a7
Mike Utz <mike.u@delegator.com>
2024-02-14 19:21:24 +0300

    feat: more resizable options
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- utzwm.lua
+++ utzwm.lua
@@ -16,16 +16,16 @@ local gridMargin = hs.geometry.size(16, 16) or {}
 hs.grid.setGrid(gridSize).setMargins(gridMargin)
 
 local definitions = {
-	{ "c", { "1,1 10x10", "2,2 8x8", "2,1 8x10", "3,1 6x10" } },
-	{ "h", { "0,0 6x12", "0,0 9x12", "0,0 3x12" } },
-	{ "i", { "6,0 6x6" } },
-	{ "j", { "0,6 12x6", "0,6 6x6", "6,6 6x6", "0,6 4x6", "4,6 4x6", "8,6 4x6" } },
-	{ "k", { "0,0 12x6", "0,0 6x6", "6,0 6x6", "0,0 4x6", "4,0 4x6", "8,0 4x6" } },
-	{ "l", { "6,0 6x12", "3,0 9x12", "9,0 3x12" } },
-	{ "m", { "6,6 6x6" } },
-	{ "n", { "0,6 6x6" } },
-	{ "return", { "0,0 12x12" } },
-	{ "u", { "0,0 6x6" } },
+	{ "c", { "1,1 10x10", "2,1 8x10", "3,1 6x10", "2,2 8x8", "3,2 6x8" } },
+	{ "h", { "0,0 9x12", "0,0 6x12", "0,0 3x12" } },
+	{ "i", { "6,0 6x6", "8,0 4x6", "10,0 2x6" } },
+	{ "j", { "0,6 12x6", "1,6 10x6", "2,6 8x6", "3,6 6x6", "4,6 4x6" } },
+	{ "k", { "0,0 12x6", "1,0 10x6", "2,0 8x6", "3,0 6x6", "4,0 4x6" } },
+	{ "l", { "3,0 9x12", "6,0 6x12", "9,0 3x12" } },
+	{ "m", { "6,6 6x6", "8,6 4x6", "10,6 2x6" } },
+	{ "n", { "0,6 6x6", "0,6 4x6", "0,6 2x6" } },
+	{ "return", { "0,0 12x12", "1,0 10x12", "2,0 8x12", "3,0 6x12", "4,0 4x12" } },
+	{ "u", { "0,0 6x6", "0,0 4x6", "0,0 2x6" } },
 }
 
 for _, definition in ipairs(definitions) do
@@ -52,15 +52,6 @@ for _, definition in ipairs(definitions) do
 		hs.grid.set(win, new_grid)
 	end)
 end
-
--- Activate grid chooser
-hs.hotkey.bind(mods, "x", function()
-	local grid = hs.grid.getGrid()
-	hs.grid.setGrid("6x4")
-	hs.grid.show(function()
-		hs.grid.setGrid(grid)
-	end)
-end)
 
 -- Center on Screen
 hs.hotkey.bind(mods, "space", function()
