48e206c2a7ee23c1781818982a76c0bc4ee68bd2
Mike Utz <mike.u@delegator.com>
2024-03-13 16:00:09 +0300

    refactor: rename modules
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@ hs.application.enableSpotlightForNameSearches(true)
 local reloadr = require "reloadr"
 reloadr.setup():start()
 
-local window_manager = require "window_manager"
-window_manager.setup()
+local windowr = require "windowr"
+windowr.setup()
 
-local hyper = require "hyper"
-hyper.setup()
+local hyperr = require "hyperr"
+hyperr.setup()
 
-local mouse_keys = require "mouse_keys"
-mouse_keys.setup()
+local mouser = require "mouser"
+mouser.setup()
 
 hs.notify
 	.new({
