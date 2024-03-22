d6441f52a4a71b9a7a234129a83dceb45068836a
Mike Utz <mike.u@delegator.com>
2024-03-22 17:13:41 +0300

    feat(deadr): better grid layout stuff
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- Spoons/Deadr.spoon/hud.lua
+++ Spoons/Deadr.spoon/hud.lua
@@ -61,114 +61,65 @@ function M:setItems(items)
 	return self
 end
 
----@private
 function M:renderCanvas()
-	local screen_frame = hs.screen.mainScreen():fullFrame()
+	local screenFrame = hs.screen.mainScreen():fullFrame()
 
-	---@diagnostic disable-next-line: param-type-mismatch
-	self.logger.vf("canvas frame: %s", hs.inspect(screen_frame))
+	local canvas = hs.canvas.new(screenFrame)--[[@as hs.canvas]]
 
-	local cells_canvas = self:buildCells()
-	local container_geo = hs.geometry.size(1200, 800)--[[@as hs.geometry]]
-	container_geo.x = (screen_frame.w - container_geo.w) / 2
-	container_geo.y = (screen_frame.h - container_geo.h) / 2
+	canvas[1] = {
+		type = "rectangle",
+		action = "fill",
+		fillColor = { hex = "#000", alpha = 0.3 },
+	}
 
-	local container_canvas = hs
-		.canvas
-		.new({}) --[[@as hs.canvas]]
-		:appendElements({
-			{
-				type = "rectangle",
-				action = "fill",
-				fillColor = { hex = "#00f", alpha = 0.5 },
-				roundedRectRadii = { xRadius = 20, yRadius = 20 },
-			},
-			{
-				type = "canvas",
-				canvas = cells_canvas,
-				frame = { x = "10%", y = "10%", w = "80%", h = "80%" },
-			},
-		})
+	canvas[2] = {
+		type = "rectangle",
+		action = "fill",
+		fillColor = { hex = "#33a", alpha = 0.8 },
+		frame = { x = 0, y = 0, w = 1200, h = 800 },
+		roundedRectRadii = { xRadius = 16, yRadius = 16 },
+	}
 
-	local overlay_canvas = hs
-		.canvas
-		.new(screen_frame) --[[@as hs.canvas]]
-		:appendElements({
-			{ type = "rectangle", fillColor = { hex = "#000", alpha = 0.2 } },
-			{
-				type = "canvas",
-				frame = container_geo.table,
-				canvas = container_canvas,
-			},
-		})
-	M.canvas = overlay_canvas
-end
+	canvas[2].frame.x = (screenFrame.w - canvas[2].frame.w) / 2
+	canvas[2].frame.y = (screenFrame.h - canvas[2].frame.h) / 2
 
----@private
----@return hs.canvas
-function M:buildCells()
-	local size = 140
-	local margin = size / 10
-	local rounded = 10
-	local cell_fill = { hex = "#000", alpha = 0.9 }
-	local text_color = { hex = "#55a" }
-	local font = "IBM Plex Mono"
+	local container = hs.geometry.new(canvas[2].frame)
+	local size = 120
+	local padding = 20
+	local gap = padding / 2
+	local max_cols = 9
+	local row = 1
+	local col = 1
 
-	local els = {}
-
-	for i, c in ipairs(self.items) do
-		local key, name = table.unpack(c)
-		local el = {
-			type = "canvas",
+	for i, item in ipairs(self.items) do
+		local curr = canvas:elementCount()
+		canvas[curr + 1] = {
+			type = "rectangle",
+			fillColor = { hex = "#000" },
 			frame = {
-				x = ((i - 1) * size) + ((i - 1) * margin * 2),
-				y = 0,
-				h = size,
+				x = container.x
+					+ padding
+					+ (col - 1) * size
+					+ (gap * (col - 1)),
+				y = container.y
+					+ padding
+					+ (row - 1) * size
+					+ (gap * (row - 1)),
 				w = size,
+				h = size,
 			},
-			canvas = hs
-				.canvas
-				.new({})--[[@as hs.canvas]]
-				:appendElements({
-					type = "rectangle",
-					action = "fill",
-					fillColor = cell_fill,
-					roundedRectRadii = {
-						xRadius = rounded,
-						yRadius = rounded,
-					},
-				}, {
-					frame = { x = "10%", y = "20%", h = "30%", w = "80%" },
-					text = hs
-						.styledtext
-						.new(key, {
-							font = { name = font .. " Bold", size = 32 },
-							color = text_color,
-							paragraphStyle = {
-								alignment = "center",
-							},
-						})--[[@as hs.styledtext]]
-						:upper(),
-					type = "text",
-				}, {
-					frame = { x = "10%", y = "60%", h = "50%", w = "80%" },
-					text = hs.styledtext.new(name, {
-						font = { name = font, size = 18 },
-						color = text_color,
-						paragraphStyle = {
-							alignment = "center",
-						},
-					}),
-					type = "text",
-				}),
+			roundedRectRadii = { xRadius = 8, yRadius = 8 },
 		}
-		table.insert(els, el)
+
+		if i % max_cols == 0 then
+			row = row + 1
+			col = 1
+		else
+			col = col + 1
+		end
 	end
 
-	return hs
-		.canvas
-		.new({}) --[[@as hs.canvas]]
-		:appendElements(els)
+	self.canvas = canvas
 end
 
 return M
