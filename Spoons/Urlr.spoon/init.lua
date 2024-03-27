febc094d2a63cef79738841e1e9ba61821629eda
Mike Utz <mike.u@delegator.com>
2024-03-27 17:09:57 +0300

    feat: allow browser to be chosen dynamically
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- Spoons/Urlr.spoon/init.lua
+++ Spoons/Urlr.spoon/init.lua
@@ -1,20 +1,7 @@
----@alias UrlrRoutes { [string]: string[] }
-
----@class (exact) Urlr
----@field public name string
----@field public version string
----@field public author string
----@field public license string
----@field public homepage string
----@field public init function fun(s: self): self
----@field public start fun(s: self): self
----@field public setDefaultBrowser fun(s: self, browser: string): self
----@field public setRoutes fun(s: self, routes: UrlrRoutes): self
----@field private http_callback fun(self): fun(scheme: string, host: string, params: {[string]: string}[], fullURL: string): nil
----@field private open_url_in_browser fun(url, browser): nil
----@field private default_browser string
----@field private routes UrlrRoutes
 local obj = {}
+
+local some = hs.fnutils.some
+local partial = hs.fnutils.partial
 
 obj.name = "Urlr"
 obj.version = "0.0.0"
@@ -26,48 +13,52 @@ obj.default_browser = "Firefox"
 
 obj.routes = {}
 
----@nodiscard
 function obj:init()
 	return self
 end
 
----@nodiscard
 function obj:start()
 	hs.urlevent.httpCallback = obj:http_callback()
 	return self
 end
 
----@nodiscard
-function obj:setDefaultBrowser(browser)
-	obj.default_browser = browser
-	return self
-end
-
----@nodiscard
-function obj:setRoutes(routes)
-	obj.routes = routes
-	return self
-end
-
----@nodiscard
 function obj.open_url_in_browser(url, browser)
-	hs.task.new("/usr/bin/open", function() end, { "-a", browser, url }):start()
+	hs.task
+		.new(
+			"/usr/bin/open",
+			function() end,
+			{ "-a", browser or self.default_browser, url }
+		)
+		:start()
 end
 
----@nodiscard
 function obj:http_callback()
 	return function(_, host, _, fullURL)
-		for browser, patterns in pairs(obj.routes) do
-			if
-				hs.fnutils.some(patterns, function(pattern)
-					return string.match(host, pattern)
-				end)
-			then
-				self.open_url_in_browser(fullURL, browser)
+		local go = partial(self.open_url_in_browser, fullURL) --[[@as function]]
+		for browser, hosts in pairs(obj.routes) do
+			if some(hosts, self.matchHost(host)) then
+				go(browser)
 				return
 			end
 		end
-		self.open_url_in_browser(fullURL, self.default_browser)
+		hs.chooser
+			.new(function(browser)
+				go(browser and browser.text)
+			end)
+			:choices(function()
+				local choices = {}
+				for k, _ in pairs(obj.routes) do
+					table.insert(choices, { text = k })
+				end
+				return choices
+			end)
+			:show()
+	end
+end
+
+function obj.matchHost(host)
+	return function(pattern)
+		return string.match(host, pattern)
 	end
 end
 
